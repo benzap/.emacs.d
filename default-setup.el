f6724e4fe21e03ec34ca1f25aead291b3c46d7dd
Benjamin Zaporzan <BZaporzan@aviationintertec.com>
2014-10-01 13:20:32 -0400

    added DOS eol removal

--- default-setup.el
+++ default-setup.el
@@ -324,6 +324,15 @@ kills the screen if true"
      '(load-library "sql-indent")
      '(setq sql-product 'ms)))
 
+;;remove '^M' from DOS encoded files
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
+(add-hook 'text-mode-hook 'remove-dos-eol)
+(add-hook 'sql-mode-hook 'remove-dos-eol)
 
 
 
