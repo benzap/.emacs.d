b2b26468f1d76b14527cc03a8ce5fba8600bc04a
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2014-10-30 09:47:14 -0400

    replace font with beginning choice

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -2,17 +2,6 @@
 (require 'package-utils)
 (require 'utils)
 
-;;On windows systems, set the default font to Consolas
-;;http://tinyurl.com/kk9eb
-(if (system-type-is-win32)
-    (progn
-      (set-default-font
-       "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")
-      (add-hook 'before-make-frame-hook
-		(lambda ()
-		  (set-frame-font 
-		   "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")))))
-
 ;;color theme
 (if (not (window-system))
     (progn 
