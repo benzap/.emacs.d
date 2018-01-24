11e8822aba3834747647e9ee5ce4b5af55ad7651
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-01-24 08:23:06 -0500

    updated erc emotes, fixed emacs server daemon

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -1,9 +1,11 @@
 ;;file includes any settings for the apperance of emacs
 (require 'package-utils)
 (require 'utils)
+(require 'server)
 
 ;;color theme
-(if (not (window-system))
+(if (and (not (window-system))
+	 (not (server-running-p)))
     (progn 
       (package-require 'color-theme)
       (color-theme-initialize)
