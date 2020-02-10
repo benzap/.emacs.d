948eea96afe1439b30787908215bdc596651715b
Benjamin Zaporzan <benzaporzan@gmail.com>
2020-02-10 11:52:46 -0500

    prevent automatic updates from taking over intialization

--- lisp/configs/auto-package-update-config.el
+++ lisp/configs/auto-package-update-config.el
@@ -7,6 +7,7 @@
   (message "Auto Package Update Configuring...")
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
+  (setq auto-package-update-prompt-before-update t)
   (auto-package-update-maybe))
 
 
