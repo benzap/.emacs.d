2d26eb41a5c7d03c17aa4b7d38082b6ccba4699a
Benjamin Zaporzan <benzaporzan@gmail.com>
2022-11-13 13:52:31 -0500

    Added fonts

--- init.el
+++ init.el
@@ -240,6 +240,20 @@
   (setq mac-control-modifier 'control)
   (global-set-key [kp-delete] 'delete-char))
 
+;; icons all the time theme
+(use-package all-the-icons
+  :ensure t
+  :if (display-graphic-p))
+
+;;(all-the-icons-install-fonts) ; Make sure the fonts are installed.
+
+
+;; Treeview
+(use-package neotree
+  :ensure t
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (global-set-key (kbd "<f8>") 'neotree-toggle))
 
 ;;
 ;; DONE
@@ -247,3 +261,38 @@
 ;; (find-file "~/.emacs.d/init.el")
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(neotree neo-tree all-the-icons true zones yasnippet web-mode use-package undo-tree toml-mode racer powerline parinfer-rust-mode org-bullets monokai-theme lispy key-chord js2-mode helm-swoop helm-smex helm-flx helm-company flycheck-rust diminish delight counsel cider cargo auto-package-update a)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:family user-default-mono-font :height 160))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro" :height 1.5 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro" :height 1.4))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro" :height 1.3))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro" :height 1.2))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#fdfdfd" :font "Source Sans Pro"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family user-default-sans-font :height 180 :weight thin)))))
